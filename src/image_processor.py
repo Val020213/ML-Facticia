181835bb7580b928c1e5c405894e17fc588629ea
Daniel Toledo <daniel020126@gmail.com>
2025-01-24 17:54:08 -0500

    image crop

--- src/image_processor.py
+++ src/image_processor.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 def extract_image(image_path, xywhr_coordinates):
     img = Image.open(image_path)
-    x, y, w, h, r = xywhr_coordinates[0].cpu().numpy()
+    x, y, w, h, r = xywhr_coordinates.cpu().numpy()
     img.rotate(-r)
     cropped_img = img.crop((x - w / 2, y - h / 2, x + w / 2, y + h / 2))
     return cropped_img
