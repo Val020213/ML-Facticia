7d183f95c92a672b2505350813ec6e61c5f75bac
Daniel Toledo <daniel020126@gmail.com>
2025-01-24 19:51:32 -0500

    format for data

---
+++ src/data_format.py
@@ -0,0 +1,8 @@
+class DataFormat:
+    
+    def __init__(self, filename, xyhwr, type, text):
+        
+        self.filename = filename
+        self.xywhr = xyhwr
+        self.type = type
+        self.text = text
