9de74a07f5aca2e91bc8ab510d5460190ba127fc
osvaldo.rmp <osvaldo.rmp@avangenio.com>
2025-02-10 22:26:39 -0500

    fix streamlit app

--- src/image_processor.py
+++ src/image_processor.py
@@ -9,8 +9,8 @@ from src.utils import preprocess_photography_image
 def extract_image(image_path, xywhr_coordinates):
     img = Image.open(image_path)
     x, y, w, h, r = xywhr_coordinates.cpu().numpy()
-    img.rotate(-r)
-    cropped_img = img.crop((x - w / 2, y - h / 2, x + w / 2, y + h / 2))
+    rotated_img = img.rotate(-r, expand=True)
+    cropped_img = rotated_img.crop((x - w / 2, y - h / 2, x + w / 2, y + h / 2))
     return cropped_img
 
 
